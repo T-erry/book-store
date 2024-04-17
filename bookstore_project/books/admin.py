f8d3da22eaa7df2247a46768dc0a4b828cd343f4
T-erry <terrywachuka10@gmail.com>
2024-04-17 15:11:18 +0300

    creaated BookAdmin

--- bookstore_project/books/admin.py
+++ bookstore_project/books/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from books.models import Book
+from books.models import Book, Author
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
 
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+
