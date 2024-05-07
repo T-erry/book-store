0d61dc20adc8324f3c494425af6a2faa8a33df47
T-erry <terrywachuka10@gmail.com>
2024-05-07 14:55:50 +0300

    created a new field user_id in Review model

--- bookstore_project/books/models.py
+++ bookstore_project/books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -22,6 +23,7 @@ def __str__(self):
     return self.title
 
 class Review(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True )
     body = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
     book = models.ForeignKey(Book, on_delete= models.CASCADE, null=True)
