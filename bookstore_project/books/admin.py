e641f4c06251117d5cd87cec40311216d5deb685
T-erry <terrywachuka10@gmail.com>
2024-05-08 15:08:40 +0300

    registered Review model in the admin

--- bookstore_project/books/admin.py
+++ bookstore_project/books/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from books.models import Book, Author
+from books.models import Book, Author, Review
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_display = ('id','title')
 
+
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
+admin.site.register(Review)
 
