05e6e0a59b4840d1233b0e11b3dd4288532db8ad
T-erry <terrywachuka10@gmail.com>
2024-05-10 15:19:04 +0300

    added a static method for my static files

--- bookstore_project/bookstore/urls.py
+++ bookstore_project/bookstore/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,7 +27,7 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(redirect_authenticated_user=True)),
     path('', include('django.contrib.auth.urls')),
     
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
   
