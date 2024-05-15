c11d2409365820ae23a49dd9044a525db8f503dd
T-erry <terrywachuka10@gmail.com>
2024-05-15 16:05:30 +0300

    uploaded form with a model form

--- bookstore_project/books/views.py
+++ bookstore_project/books/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404,redirect
 from books.models import Book, Review 
 from django.views.generic import ListView, DetailView
 from django.core.files.storage import FileSystemStorage
+from books.forms import ReviewForm
 # from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -34,6 +35,7 @@ class BookDetailView(DetailView):
            
             context['reviews'] = context['book'].review_set.order_by('-created_at')
             context['authors'] = context['book'].authors.all()
+            context ['form'] = ReviewForm()
             return context
             
     
@@ -58,12 +60,15 @@ def author(request,author):
 #POST method
 def review(request, id):
     if request.user.is_authenticated:
-        image = request.FILES['image']
-        fs = FileSystemStorage()
-        name = fs.save( image.name,image)
-   #  create a form instance and populate it with data from the request:
-        body = (request.POST['review'])
-        newReview = Review(body=body, book_id=id, user=request.user, image=fs.url(name))
-        newReview.save()
-        return redirect('/books')
-    
+         newReview = Review(book_id=id, user=request.user)
+           #  create a form instance and populate it with data from the request:
+         form = ReviewForm(request.POST,request.FILES, instance=newReview)
+         if form.is_valid():
+            form.save()   
+         else:
+              print('Form is not valid')
+
+    return redirect(f"/books/{id}")
+   
+
+   
